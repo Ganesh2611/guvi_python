c9d69b2a2b9a72365d7136a5a9fe164f872ff511
Ganesh2611 <52314009+Ganesh2611@users.noreply.github.com>
2019-06-28 15:10:06 +0530

    Create words.py

---
+++ words.py
@@ -0,0 +1,7 @@
+gk=input()
+if gk in ('a','e','i','o','u'):
+   print("Vowel")
+elif gk in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+    print("Consonant")
+else: 
+   print("Invalid")
